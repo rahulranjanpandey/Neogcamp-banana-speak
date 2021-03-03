70c955399b9556b31a45d5722375ee76673b5f3d
Rahul Ranjan Pandey <62777073+rahulranjanpandey@users.noreply.github.com>
2021-03-03 17:35:58 +0530

    commit to main

---
+++ app.js
@@ -0,0 +1,11 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.quesrySelector("#output");
+
+
+function clickHandler() {
+    outputDiv.innerText = ajajajjajaj
+};
+
+btnTranslate.addEventListener("click", clickHandler)
+ 
