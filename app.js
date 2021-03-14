8fdb2e932996fd9207acbcef91b664845d71fff0
Rahul Ranjan Pandey <62777073+rahulranjanpandey@users.noreply.github.com>
2021-03-15 03:00:21 +0530

    commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.quesrySelector("#output");
+var outputDiv = document.querySelector("#output");
 
 // var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
